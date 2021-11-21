89bdde2c1822c4a168de1e734f499f15ccfad5b8
ralph <rbg619@usask.ca>
2021-11-21 15:33:38 -0600

    Adopt a pet page is finished. Just need to implement updating pets

--- frontend/client/src/components/createUser.js
+++ frontend/client/src/components/createUser.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Form, Button } from 'react-bootstrap';
 
 const Create = () => (
-    <Form className="create-form">
-        <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-        </Form.Field>
-        <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
+    <Form>
+        <Form.Group className="mb-3" controlId="formFname">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="fname" placeholder="fname" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formLname">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="Lname" placeholder="Lname" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+        </Form.Group>
     </Form>
 )
 
